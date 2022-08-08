8c68cf964b555ba17e45fb68a317a64e7f63a234
Petya <bomj.8omj@yandex.ru>
2022-08-08 21:24:46 +0300

    Added tests files

---
+++ tests/private.example.php
@@ -0,0 +1,20 @@
+<?php
+
+//
+// ВНИМАНИЕ!!!
+// Это пример private.php, в котором хранятся
+// приватные данные, которые лучше не загружать в репозиторий.
+//
+
+return [
+    "type" => "mysql",
+
+    "charset" => "utf8mb4",
+
+    "host" => "localhost",
+
+    "username" => "root",
+    "password" => "",
+
+    "database" => "",
+];
