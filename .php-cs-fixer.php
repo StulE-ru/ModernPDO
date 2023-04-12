c06ec820fe92da6b6449a5dc8fda9759fdfd699e
Petya <bomj.8omj@yandex.ru>
2023-04-13 02:23:58 +0300

    Create .php-cs-fixer.php

---
+++ .php-cs-fixer.php
@@ -0,0 +1,63 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = (new Finder())
+    ->in(__DIR__)
+    ->exclude([
+        'vendor',
+    ]);
+
+return (new Config())
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+
+        // Alias
+
+        'array_push' => true,
+        'backtick_to_shell_exec' => true,
+        'modernize_strpos' => true,
+        'no_alias_functions' => true,
+        'no_mixed_echo_print' => ['use' => 'print'],
+        'pow_to_exponentiation' => true,
+        'random_api_migration' => true,
+        'set_type_to_cast' => true,
+
+        // Array Notation
+
+        'array_syntax' => ['syntax' => 'short'],
+        'no_multiline_whitespace_around_double_arrow' => true,
+        'no_trailing_comma_in_singleline_array' => true,
+        'no_whitespace_before_comma_in_array' => true,
+        'normalize_index_brace' => true,
+        'trim_array_spaces' => true,
+        'whitespace_after_comma_in_array' => ['ensure_single_space' => true],
+
+        // Other...
+
+        'align_multiline_comment' => ['comment_type' => 'phpdocs_like'],
+        'combine_consecutive_unsets' => true,
+        'concat_space' => [
+            'spacing' => 'one',
+        ],
+        'heredoc_to_nowdoc' => true,
+        'no_unreachable_default_argument_value' => true,
+        'no_useless_return' => true,
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+            'imports_order' => [
+                'class',
+                'const',
+                'function',
+            ],
+        ],
+        'ternary_to_null_coalescing' => true,
+        'yoda_style' => [
+            'equal' => false,
+            'identical' => false,
+            'less_and_greater' => false,
+        ],
+    ]);
