5d5dcc9b93a10c53eef0972cfa4dba6015a7c963
Petya <bomj.8omj@yandex.ru>
2023-08-18 15:18:25 +0300

    Add test cases

---
+++ tests/Unit/StatementTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ModernPDO\Tests\Unit;
+
+use ModernPDO\Statement;
+use PDOStatement;
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+
+class StatementTest extends TestCase
+{
+    public function testNull(): void
+    {
+        $st = new Statement(null);
+
+        assertEquals(0,  $st->rowCount());
+        assertEquals([],  $st->fetch());
+        assertEquals([],  $st->fetchAll());
+    }
+
+    public function testInvalid(): void
+    {
+        $st = new Statement(new PDOStatement());
+
+        assertEquals(0,  $st->rowCount());
+        assertEquals([],  $st->fetch());
+        assertEquals([],  $st->fetchAll());
+    }
+}
